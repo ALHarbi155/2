52f87e6e25a0b171df3722da7d8a926e5209f24c
ALHarbi155 <59547926+ALHarbi155@users.noreply.github.com>
2020-06-25 18:37:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`@!ALHarbi`,"")
+client.user.setGame(`@!ALHarbi`,"https://ALHarbi.com")
 client.user.setStatus("dnd")
 });
 
